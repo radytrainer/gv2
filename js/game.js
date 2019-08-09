9feb8ab465f20798782c76f995dba58e1df792ce
rady y <rady.y@passerellesnumeriques.org>
2019-08-09 11:15:13 +0700

    Update game.js
    Testing 1

--- js/game.js
+++ js/game.js
@@ -8,59 +8,59 @@ var khmerGame = () => {
 
     switch (mix1) {
         case 1:
-            output1 = "<img src='../images/1.PNG' style='width:100px;'>";
+            output1 = "<img src='images/1.PNG' style='width:100px;'>";
             break;
         case 2:
-            output1 = "<img src='../images/2.PNG' style='width:100px;'>";
+            output1 = "<img src='images/2.PNG' style='width:100px;'>";
             break;
         case 3:
-            output1 = "<img src='../images/3.PNG' style='width:100px;'>";
+            output1 = "<img src='images/3.PNG' style='width:100px;'>";
             break;
         case 4:
-            output1 = "<img src='../images/4.PNG' style='width:100px;'>";
+            output1 = "<img src='images/4.PNG' style='width:100px;'>";
             break;
         case 5:
-            output1 = "<img src='../images/5.PNG' style='width:100px;'>";
+            output1 = "<img src='images/5.PNG' style='width:100px;'>";
             break;
         case 6:
-            output1 = "<img src='../images/6.PNG' style='width:100px;'>";
+            output1 = "<img src='images/6.PNG' style='width:100px;'>";
             break;
     }
     switch (mix2) {
         case 1:
-            output2 = "<img src='../images/1.PNG' style='width:100px;'>";
+            output2 = "<img src='images/1.PNG' style='width:100px;'>";
             break;
         case 2:
-            output2 = "<img src='../images/2.PNG' style='width:100px;'>";
+            output2 = "<img src='images/2.PNG' style='width:100px;'>";
             break;
         case 3:
-            output2 = "<img src='../images/3.PNG' style='width:100px;'>";
+            output2 = "<img src='images/3.PNG' style='width:100px;'>";
             break;
         case 4:
-            output2 = "<img src='../images/4.PNG' style='width:100px;'>";
+            output2 = "<img src='images/4.PNG' style='width:100px;'>";
             break;
         case 5:
-            output2 = "<img src='../images/5.PNG' style='width:100px;'>";
+            output2 = "<img src='images/5.PNG' style='width:100px;'>";
             break;
         case 6:
-            output2 = "<img src='../images/6.PNG' style='width:100px;'>";
+            output2 = "<img src='images/6.PNG' style='width:100px;'>";
             break;
     }
     switch (mix3) {
         case 1:
-            output3 = "<img src='../images/1.PNG' style='width:100px;'>";
+            output3 = "<img src='images/1.PNG' style='width:100px;'>";
             break;
         case 2:
-            output3 = "<img src='../images/2.PNG' style='width:100px;'>";
+            output3 = "<img src='images/2.PNG' style='width:100px;'>";
             break;
         case 3:
-            output3 = "<img src='../images/3.PNG' style='width:100px;'>";
+            output3 = "<img src='images/3.PNG' style='width:100px;'>";
             break;
         case 4:
-            output3 = "<img src='../images/4.PNG' style='width:100px;'>";
+            output3 = "<img src='images/4.PNG' style='width:100px;'>";
             break;
         case 5:
-            output3 = "<img src='../images/5.PNG' style='width:100px;'>";
+            output3 = "<img src='images/5.PNG' style='width:100px;'>";
             break;
         case 6:
             output3 = "<img src='../images/6.PNG' style='width:100px;'>";
